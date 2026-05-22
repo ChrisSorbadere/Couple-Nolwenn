ac4deae225022ef537186e4408fcac018897e77c
ChrisSorbadere <chrissorbadere@gmail.com>
2026-05-22 18:15:00 +0200

    Add files via upload

---
+++ sw-fr.js
@@ -0,0 +1,13 @@
+const CACHE = 'nous-deux-v1';
+const ASSETS = ['./index.html', './manifest-fr.json', './icon-fr-192.png', './icon-fr-512.png', './icon-fr-180.png', './favicon-fr-32.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
